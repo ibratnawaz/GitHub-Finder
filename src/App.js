22f61d31ab90254a5847c0068311b3a3b7bfaef4
Ibrat Nawaz <ibratrocker99@gmail.com>
2020-12-26 23:07:20 +0530

    deployed to netlify

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:username' component={User} />
-                <Route component={NotFound} />
+                <Route path='*' component={NotFound} />
               </Switch>
             </div>
           </div>
